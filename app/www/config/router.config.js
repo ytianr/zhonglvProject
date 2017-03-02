a9256b5cc55a9aa491f6afa9b57c7d20347303b3
ytian <1076821345@qq.com>
2017-03-02 11:54:00 +0800

    课程

--- app/www/config/router.config.js
+++ app/www/config/router.config.js
@@ -16,7 +16,36 @@ angular.module( "lotour.app").config(function( $stateProvider,$urlRouterProvider
 		templateUrl : "controllers/ledge/ledge.html",
 		controller : "ledgeCtrl"
 	} )
-
+	.state('kecheng',{
+      url: '/kecheng',
+      templateUrl: 'controllers/kecheng/kecheng.html',
+      controller: 'kechengCtrl'
+    })
+    .state('kecheng.kc1',{
+    	url: '/kc1',
+    	templateUrl: 'controllers/kecheng/kecheng/kc1.html',
+    	controller: 'kechengCtrl'
+    })
+    .state('kecheng.kc2',{
+    	url: '/kc2',
+    	templateUrl: 'controllers/kecheng/kecheng/kc2.html',
+    	controller: 'kechengCtrl'
+    })
+    .state('kecheng.kc3',{
+    	url: '/kc3',
+    	templateUrl: 'controllers/kecheng/kecheng/kc3.html',
+    	controller: 'kechengCtrl'
+    })
+    .state('kecheng.kc4',{
+    	url: '/kc4',
+    	templateUrl: 'controllers/kecheng/kecheng/kc4.html',
+    	controller: 'kechengCtrl'
+    })
+    .state('kc_detail',{
+        url: '/kc_detail',
+        templateUrl: 'controllers/kc_detail/kc_detail.html',
+        controller: 'kechengCtrl'
+    })
 	
 
 	$urlRouterProvider.otherwise( "/home" );
